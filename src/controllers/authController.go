c8b8f563ea4c24a3f8b2aa87eeaf29c33cce5db5
dan2k <sangtong@gmail.com>
2022-07-01 07:21:44 +0000

    finish profile

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -24,7 +24,6 @@ func Register(c *fiber.Ctx) error {
 			"message": "password do not match",
 		})
 	}
-	
 
 	user := models.User{
 		FirstName:    data["first_name"],
@@ -33,7 +32,7 @@ func Register(c *fiber.Ctx) error {
 		IsAmbassador: false,
 	}
 	user.SetPassword(data["password"])
-	if err :=database.DB.Create(&user); err !=nil{
+	if err := database.DB.Create(&user); err != nil {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON(fiber.Map{
 			"error": err.Error,
@@ -41,45 +40,44 @@ func Register(c *fiber.Ctx) error {
 	}
 	return c.JSON(user)
 }
-func Login(c *fiber.Ctx) error{
+func Login(c *fiber.Ctx) error {
 	var data map[string]string
 	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
 
 	var user models.User
-	database.DB.Where("email = ?",data["email"]).First(&user)
-	if user.Id==0{
+	database.DB.Where("email = ?", data["email"]).First(&user)
+	if user.Id == 0 {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON(fiber.Map{
 			"message": "Invalid Credentials 1",
 		})
 	}
 
-	if err :=user.ComparePassword(data["password"]); err !=nil{
+	if err := user.ComparePassword(data["password"]); err != nil {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON(fiber.Map{
 			"message": "Invalid Credentials 2",
 		})
 	}
 
-	payload :=jwt.StandardClaims{
-		Subject: strconv.Itoa(int(user.Id)),
-		ExpiresAt: time.Now().Add(time.Hour *24).Unix(),
-
+	payload := jwt.StandardClaims{
+		Subject:   strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
 	}
-	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256,payload).SignedString([]byte("secret"))
-	if  err !=nil{
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte("secret"))
+	if err != nil {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON(fiber.Map{
 			"message": "Invalid Credentials 3",
-			"error": err.Error(),
+			"error":   err.Error(),
 		})
 	}
 	cookie := fiber.Cookie{
-		Name: "jwt",
-		Value: token,
-		Expires: time.Now().Add(time.Hour *24),
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
 		HTTPOnly: true,
 	}
 	c.Cookie(&cookie)
@@ -89,22 +87,60 @@ func Login(c *fiber.Ctx) error{
 	})
 
 }
-func User(c *fiber.Ctx) error{
-	
+func User(c *fiber.Ctx) error {
+
 	var user models.User
-	id,_ := middlewares.GetUserId(c)
-	database.DB.Where("id = ?",id).First(&user)
+	id, _ := middlewares.GetUserId(c)
+	database.DB.Where("id = ?", id).First(&user)
 	return c.JSON(user)
 }
-func Logout(c *fiber.Ctx) error{
-	cookie:=fiber.Cookie{
-		Name: "jwt",
-		Value: "",
-		Expires: time.Now().Add(time.Hour),
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(time.Hour),
 		HTTPOnly: true,
 	}
 	c.Cookie(&cookie)
 	return c.JSON(fiber.Map{
-		"message":"success",
+		"message": "success",
 	})
+}
+func UpdateInfo(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	id, _ := middlewares.GetUserId(c)
+	user := models.User{
+		Id:        id,
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+	}
+
+	database.DB.Model(&user).Updates(&user)
+	return c.JSON(user)
+}
+func UpdatePassword(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "password do not match",
+		})
+	}
+
+
+	id, _ := middlewares.GetUserId(c)
+	user := models.User{
+		Id:        id,
+	}
+	user.SetPassword(data["password"])
+
+	database.DB.Model(&user).Updates(&user)
+	return c.JSON(user)
 }
