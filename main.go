2a043fff88ca678526e656a0fc8ebf51de3e5639
dan2k <sangtong@gmail.com>
2022-07-01 03:57:24 +0000

    finish register

--- main.go
+++ main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"ambassador/src/database"
+	"ambassador/src/routes"
 	"log"
+
 	"github.com/gofiber/fiber/v2"
-	
 )
 
 func main() {
 	database.Connect()
 	database.AutoMigrate()
     app := fiber.New()
-
-    app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+    routes.Setup(app)
 
     log.Fatal(app.Listen(":8080"))
 }
