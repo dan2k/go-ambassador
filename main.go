5c83596ebd0cf0b411a448677395ef78179b6b8f
dan2k <sangtong@gmail.com>
2022-07-01 03:21:30 +0000

    6 live reloading air

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-    "log"
-
-    "github.com/gofiber/fiber/v2"
-	"gorm.io/driver/mysql"
-  	"gorm.io/gorm"
+	"ambassador/src/database"
+	"log"
+	"github.com/gofiber/fiber/v2"
+	
 )
 
 func main() {
-	dsn := "root:root@tcp(db:3306)/ambassador?charset=utf8mb4&parseTime=True&loc=Local"
-  	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err !=nil {
-		panic("Could not connect database")
-	}
+	database.Connect()
+	database.AutoMigrate()
     app := fiber.New()
 
     app.Get("/", func (c *fiber.Ctx) error {
