21addb2b0608e1c47f68c0999a1376f4b1f80539
dan2k <sangtong@gmail.com>
2022-07-02 08:27:50 +0000

    caching products

--- src/controllers/productController.go
+++ src/controllers/productController.go
@@ -3,7 +3,10 @@ package controllers
 import (
 	"ambassador/src/database"
 	"ambassador/src/models"
+	"context"
+	"encoding/json"
 	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -50,4 +53,24 @@ func DeleteProduct(c *fiber.Ctx) error{
 	product.Id=uint(id)
 	database.DB.Delete(&product)
 	return nil
+}
+func ProductsFrontend(c *fiber.Ctx) error{
+	var products []models.Product
+	var ctx = context.Background()
+	result,err :=database.Cache.Get(ctx,"products_frontend").Result()
+	if err !=nil{
+		database.DB.Find(&products)
+		bytes,err := json.Marshal(products)
+		if err != nil{
+			panic(err)
+		}
+
+		if errKey:= database.Cache.Set(ctx,"products_frontend",bytes,time.Minute*30);errKey !=nil{
+			panic(errKey)
+		}
+		
+	} else {
+		json.Unmarshal([]byte(result),&products)
+	}
+	return c.JSON(products)
 }
